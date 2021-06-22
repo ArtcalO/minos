e72cf27fcd3727fedd194360f66160c0b7e67a1a
Herve2iko <ihervedelice@gmail.com>
2021-06-22 20:00:32 +0200

    minos api

--- minos/static/minos.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#tableRows tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
--- minosproject/settings.py
+++ minosproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'minos',
+    'api_minos',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
